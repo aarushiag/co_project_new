e6a171202cb5f64c6a64b5137cc79c9a78970b84
pragya16067 <pragya16067@iiitd.ac.in>
2017-11-28 16:01:23 +0530

    Fetch done, Decode ongoing

--- Main.c
+++ Main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 static unsigned long int MEM[1000];
-static unsigned int address[1000];
-static unsigned int instruction[1000];
+static unsigned int address;
+static unsigned int instruction;
 //stores the address and instruction of the current line being read from file
 //used in Fetch function and ReadFromFile function
-static unsigned int current_address,current_instruction;
-int PC=0;
-int flag=0;
+static unsigned int current_address;
+static unsigned int PC=0;
+static unsigned int current_instruction;
+
+static unsigned int Condition;
+static unsigned int Flag;
+static unsigned int Immediate;
+static unsigned int Opcode;
+static unsigned int Operand1;
+static unsigned int Operand2;
+static unsigned int Destination;
 
 
 void ReadFromFile() {
@@ -16,7 +26,6 @@ void ReadFromFile() {
 	
 	if(fp == NULL) {
 		printf("Error opening file for writing\n");
-		flag=-1;
 		return;
   }
 	int ct=0;
@@ -32,18 +41,29 @@ void ReadFromFile() {
 void Fetch() {
 	current_address=PC;
 	current_instruction=MEM[PC];
-	printf("%s %x %s %x","Fetching instruction ",current_instruction," from address ",current_address);
+	printf("%s %x %s %x \n","Fetching instruction ",current_instruction," from address ",current_address);
 	PC+=4;
 }
+
+
+void ReadInstruction() {
+	//printf("%x",current_instruction);
+	Condition = (current_instruction >> 28) & (0b1111);
+	printf("%d \n",Condition);
+	Flag = (current_instruction >> 26) & (0b0011);
+	printf("%d \n",Flag);
+}
+
+void decode() {
+	//current_instruction= 0xE3A0200A;
+	ReadInstruction();
+	
+}
+
 	
 void main() {
-	printf("Hello");
 	ReadFromFile();
-	int ct=0;
-	if (flag==0){
-	while(ct<4){
-		Fetch();
-		ct++;
-	}
-	}
+	Fetch();
+	decode();
+	
 }
