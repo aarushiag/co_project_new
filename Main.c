c9a2b19e82e862a9fd30de765e86dce03cb09d82
pragya16067 <pragya16067@iiitd.ac.in>
2017-11-28 17:08:38 +0530

    Add files via upload

--- Main.c
+++ Main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-#include <stdio.h>
-
 static unsigned long int MEM[1000];
 static unsigned int address;
 static unsigned int instruction;
@@ -14,10 +12,11 @@ static unsigned int Condition;
 static unsigned int Flag;
 static unsigned int Immediate;
 static unsigned int Opcode;
+static unsigned int Offset;
 static unsigned int Operand1;
 static unsigned int Operand2;
 static unsigned int Destination;
-static int register[15];
+static unsigned int R[16];
 
 void ReadFromFile() {
 	FILE *fp;
@@ -40,7 +39,7 @@ void ReadFromFile() {
 void Fetch() {
 	current_address=PC;
 	current_instruction=MEM[PC];
-	printf("%s %x %s %x \n","Fetching instruction ",current_instruction," from address ",current_address);
+	printf("%s %x %s %x \n","FETCH: Fetching instruction ",current_instruction," from address ",current_address);
 	PC+=4;
 }
 
@@ -79,33 +78,133 @@ void ReadInstruction() {
 
     else if (Flag==2)
     {
-        Opcode = ((inst>>24)&(0b0011));
-        Offset = (inst&(0b111111111111111111111111));
+        Opcode = ((current_instruction>>24) & (0b0011));
+        Offset = (current_instruction & (0b111111111111111111111111));
     }
 
 }
+
+
 void Decode() {
 	//current_instruction= 0xE3A0200A;
 	ReadInstruction();
+	if(Flag==0)
+	{
+		if(Immediate==0){
+        
+			if(Opcode==0) {
+			  printf("DECODE: Operation is AND, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==1) {
+				printf("DECODE: Operation is XOR, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==2) {
+				printf("DECODE: Operation is SUB, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==4) {
+				printf("DECODE: Operation is ADD, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==5) {
+				printf("DECODE: Operation is ADC, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==10) {
+				printf("DECODE: Operation is CMP, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==12) {
+				printf("DECODE: Operation is ORR, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==13) {
+				printf("DECODE: Operation is MOV, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==15) {
+				printf("DECODE: Operation is MNV, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+
+      }
+
+      else if(Immediate==1){
+        
+        if(Opcode==0) {
+			  printf("DECODE: Operation is AND, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==1) {
+				printf("DECODE: Operation is XOR, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==2) {
+				printf("DECODE: Operation is SUB, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==4) {
+				printf("DECODE: Operation is ADD, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==5) {
+				printf("DECODE: Operation is ADC, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==10) {
+				printf("DECODE: Operation is CMP, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==12) {
+				printf("DECODE: Operation is ORR, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==13) {
+				printf("DECODE: Operation is MOV, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+			else if(Opcode==15) {
+				printf("DECODE: Operation is MNV, First Operand is R%d, Second Operand is R%d, Destination register is R%d.\nRead Rs: R%d = %d, R%d = %d\n",Operand1,Operand2,Destination,Operand1,R[Operand1],Operand2,R[Operand2]);
+			}
+
+      }
+	}
 	
 }
 void Add(){
-	if (Immediate==0)register[Destination]=register[Operand1]+register[Operand2];
-	else register[Destination]=register[Operand1]+Operand2;
+	if(Immediate==0) 
+	{R[Destination]=R[Operand1]+R[Operand2];}
+	else 
+	{R[Destination]=R[Operand1]+Operand2;}
 }	
 void Sub(){
-	if (Immediate==0)register[Destination]=register[Operand1]-register[Operand2];
-	else register[Destination]=register[Operand1]-Operand2;
+	if(Immediate==0)
+	{R[Destination]=R[Operand1]-R[Operand2];}
+	else 
+	{R[Destination]=R[Operand1]-Operand2;}
 }
 void Mov(){
-	register[Destination]=register[Operand1];
+	R[Destination]=R[Operand1];
 }
 void Mnv(){
-	register[Destination]=~register[Operand1];
+	R[Destination]= ~R[Operand1];
 }
-void main() {
-	ReadFromFile();
-	Fetch();
-	decode();
+
+void And() {
+	if(Immediate==0)
+	{
+		R[Destination] = R[Operand1] & R[Operand2];
+	}
+	else
+	{
+		R[Destination] = R[Operand1] & Operand2;
+	}
+}
+
+void Or() {
+	if(Immediate==0)
+	{
+		int or = R[Operand1] | R[Operand2];
+		R[Destination] = or;
+	}
+	else
+	{
+		int or = R[Operand1] | Operand2;
+		R[Destination] = or;
+	}
+}
 	
+void main() {
+	
+	ReadFromFile();
+	
+	while(current_instruction != 0xEF000011) {
+		Fetch();
+		Decode();
+	}
 }
